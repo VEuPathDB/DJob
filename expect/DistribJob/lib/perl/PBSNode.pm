7eab6670581a5c575e0086e98ee4a35b028c6219
Mark Heiges <mheiges@gmail.com>
2005-05-06 17:00:59 +0000

    Use Expect.pm to monitor jobs submitted through interactive qsub (qsub -I).
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches@3790 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- expect/DistribJob/lib/perl/PBSNode.pm
+++ expect/DistribJob/lib/perl/PBSNode.pm
@@ -4,77 +4,51 @@ use DJob::DistribJob::Node;
 use CBIL::Util::Utils;
 use IPC::Open2;
 use Symbol;
+use lib '/scratch/mheiges/testbed';
 
 @ISA = qw(DJob::DistribJob::Node);
 
+use Expect;
 use strict;
 
 my $endMatchString = "FooCmdEnd";
 my $endCmdString = "echo $?.$endMatchString";
+
 my $log = $ENV{HOME} . "/distribjob.log";
 
 sub new {
     my ($class, $nodeNum, $nodeDir, $slotCount, $initDir) = @_;
-    my $self = {};  ##&DJob::DistribJob::Node::new($class, $nodeNum, $nodeDir, $slotCount);
+    my $self = {};
     bless($self, $class);
+    $self->{expect} = Expect->new();
+
     $self->{nodeDir} = "$nodeDir/$nodeNum";
     $self->{nodeNum} = $nodeNum;
     $self->{slotCount} = $slotCount;
-    my $callstr = "ssh -qx $self->{nodeNum} bash --login -s";
-
-    $self->{cmdPid} = open2($self->{readH}, $self->{writeH}, $callstr);  
-
-    return undef if $self->{cmdPid} =~ /^open2/;
-    ##set file handles to flush immediately
-    my $oldfh = select($self->{readH});
-    $| = 1;
-    select($self->{writeH});
-    $| = 1;
-    select $oldfh;
-
-    return undef unless $self->checkNode();
-
-    $self->_initNodeDir(); 
+    ##MARK, change the next line of code to work with PBS.  You should reserve all processors on a single machine
+    ##opens channel to node
     
-    return $self;
-}
-
-sub _log {
-    my $self = shift;
-    my $msg = shift;
-    open(LOG, ">>$log");
-    print LOG $msg . "\n";
-    close LOG;
-}
-
-## following the setup of a local node 
-sub _initNodeDir {
-    my($self) = @_;
-
-    if (-e $self->{nodeDir}) {
-	$self->runCmd("/bin/rm -rf $self->{nodeDir}");
+    $self->{expect}->spawn("qsub -I -l nodes=1") or die "Could not start qsub: $!\n";
+    unless ($self->{expect}->expect(undef, "mnode")) {
     }
-    $self->runCmd("mkdir -p $self->{nodeDir}");
-}
-
-## return the node number when using PBS
-sub getNum {
-  my $self = shift;
-  if(!defined $self->{nodeNum}){
-    my $hostname = $self->runCmd('hostname');
-    if($hostname =~ /^(mnode\d+)/){
-      $self->{nodeNum} = $1;
-    }else{
-      print STDERR "Unable to determine the Node number\n";
+    unless ($e->expect(10, '-re', '\$ $')) {
+        print "ERROR";
+        die "qsub did not return a prompt in a timely manner. I'm not waiting any longer.\n";
     }
-  }
-  return $self->{nodeNum};
+    if(!$initDir){
+       return undef unless $self->_initNodeDir();  
+    }
+  
+    #$self->_log(__FILE__ . ":" . __LINE__ . " PBSNode created."); # DEBUG
+    $self->runCmd("hostname > /scratch/mheiges/distribjob.test.$nodeNum");# DEBUG
+  
+  return $self;
 }
 
 sub runCmd {
     my ($self, $cmd) = @_;
-    my $cmdHandle = $self->{writeH}; 
-#    print $cmdHandle "$cmd$endCmdString";
+    my $cmdHandle = $self->{expect}; 
+    $cmdHandle->clear_accum();
     print $cmdHandle "$cmd\n";
     print $cmdHandle "$endCmdString\n";
     my $resHandle = $self->{readH};
@@ -87,6 +61,41 @@ sub runCmd {
 	$res .= $_;
     }
     return $res;
+}
+
+
+sub _log {
+    my $self = shift;
+    my $msg = shift;
+    open(LOG, ">>$log");
+    print LOG $msg . "\n";
+    close LOG;
+}
+
+##is essentially like local node for you
+sub _initNodeDir {
+    my($self) = @_;
+    if (-e $self->{nodeDir}) {
+	$self->runCmd("/bin/rm -r $self->{nodeDir}");
+    $self->_log(__FILE__ . ":" . __LINE__ . " removing : " . $self->{nodeDir}); # DEBUG
+    }
+    $self->_log(__FILE__ . ":" . __LINE__ . " creating: " . $self->{nodeDir}); # DEBUG
+    $self->runCmd("mkdir -p $self->{nodeDir}");
+}
+
+##over ride so make specific to QrshNode
+##MARK, you will need to alter this to return the node number when using PBS
+sub getNum {
+  my $self = shift;
+  if(!defined $self->{nodeNum}){
+    my $hostname = $self->runCmd('hostname');
+    if($hostname =~ /^(mnode\d+)/){
+      $self->{nodeNum} = $1;
+    }else{
+      print STDERR "Unable to determine the Node number\n";
+    }
+  }
+  return $self->{nodeNum};
 }
 
 sub execSubTask {
@@ -110,18 +119,45 @@ sub _fileExists {
 
 sub checkNode {
   my($self) = @_;
-  # a sleep process should be running through the PBS queue
-  return $self->runCmd( "ps  -C sleep --no-heading");
+  return 1; ##don't check unless there are problems
+}
+
+sub tryCommand {
+  my($self,$cmd,$retVal,$test) = @_;
+  my $pid;
+  my $hit = 0;
+  eval {
+    local $SIG{'ALRM'} = sub { die "alarm\n"; };
+    alarm(5);
+    $pid = open(BPSH, "$cmd |");
+    if (defined($pid)) {
+      while (<BPSH>) {
+        if(/$retVal/){
+          $hit = 1;
+        }
+      }
+      alarm(0);
+    } else {
+      die "qrsh error";
+    }
+  };
+  if($@){
+    if ($@ =~ /alarm/) {
+      kill('KILL', $pid);
+#      print STDERR "KILL: $@\n";
+    }else{
+      print STDERR "Something bad happened: $@\n";
+    }
+  }
+  return $hit ? $test : !$test; 
 }
 
 # over ride the cleanUp method so do the waitpid thing...
 sub cleanUp {
     my ($self) = @_;
     unless ($self->{clean}) {
-      if (-e $self->{nodeDir}) {
-          print "removing " . $self->{nodeDir} . "\n";
-          $self->runCmd("/bin/rm -rf $self->{nodeDir}");
-      }
+      print "Cleaning up node ".$self->getNum()."\n";
+      $self->runCmd("/bin/rm -r $self->{nodeDir}");
       close $self->{readH};
       close $self->{writeH};
       $self->{clean} = 1;
@@ -131,7 +167,5 @@ sub cleanUp {
     }
 }
 
-sub DESTROY {
-    &cleanup();
-}
+
 1;
