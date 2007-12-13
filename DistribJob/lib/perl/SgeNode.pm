a9bea0fd29c50c2643cbeda49fb5e5ec0b1e80e1
Brian Brunk <brunkb@upenn.edu>
2007-12-13 14:19:22 +0000

    SgeNode  updated
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@5868 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJob/lib/perl/SgeNode.pm
+++ DistribJob/lib/perl/SgeNode.pm
@@ -25,11 +25,14 @@ sub queueNode {
       close R;
       system("chmod +x $runFile");
     }
-    my $qsubcmd = "qsub -V -cwd -pe smp 2 $runFile";
+    my $qsubcmd = "qsub -V -cwd -pe DJ $self->{procsPerNode} $runFile";
+#    my $qsubcmd = "qsub -V -cwd $runFile";
     my $tjid = `$qsubcmd`;
     if($tjid =~ /job\s(\d+)/){
       my $jid = $1;
       $self->setJobid($jid);
+##NOTE: am changing so that now will use the $TMPDIR for the nodeDir so that PBS will clean up.
+      $self->{nodeDir} = "/tmp/$jid";
       open(C,">>$self->{fileName}");
       print C "$self->{jobid} ";
       close C;
@@ -53,6 +56,51 @@ sub getNodeAddress {
     }
   }
   return $self->{nodeNum};
+}
+##over ride this because want to delete those pesky *.OU files
+sub cleanUp {
+  my ($self,$force, $state) = @_;
+
+  return if $self->getState() >= $COMPLETE; #already cleaned up
+    
+  if (!$force) {
+    foreach my $slot (@{$self->getSlots()}) {
+      return unless $slot->isFinished();
+    }
+  }
+  
+  ##want to kill any child processes still running to quit cleanly
+  if($self->getState() == $INITIALIZINGTASK && $self->{taskPid}){
+    kill(1, $self->{taskPid}) unless waitpid($self->{taskPid},1);
+  }
+
+  if($state != $FAILEDNODE){  ## if the node has failed don't want to run commands on it ...
+  
+    print "Cleaning up node $self->{nodeNum}...\n";
+    
+    my $task = $self->getTask();
+    $task->cleanUpNode($self) if $task;
+    
+    
+    if($self->{nodeNum} && $self->getPort()){
+      $self->runCmd("/bin/rm -r $self->{nodeDir}", 1);
+      $self->runCmd("closeAndExit",1);
+      $self->closePort();
+      system("qdel $self->{jobid} > /dev/null 2>&1");
+    }else{
+      system("qdel $self->{jobid} > /dev/null 2>&1");
+    }
+  }
+
+  $self->setState($state ? $state : $COMPLETE); ##complete
+  
+  ##delete those pesky OU files that don't do anything
+  my $jid = $self->{jobid};
+  if($self->{jobid} =~ /^(\d+)/){
+    $jid = $1;
+  }
+  my $delCmd = "/bin/rm nodeScript.*.?$jid > /dev/null 2>&1";
+  system($delCmd); 
 }
 
 1;
