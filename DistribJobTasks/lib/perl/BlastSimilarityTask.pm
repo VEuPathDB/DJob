26000a80c5309f288351c10c7cbaa97a2eda3c19
Deborah Pinney <pinney@upenn.edu>
2009-07-16 20:17:11 +0000

    update to pass new arg to BlastSimilarity
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@7249 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/lib/perl/BlastSimilarityTask.pm
+++ DistribJobTasks/lib/perl/BlastSimilarityTask.pm
@@ -150,12 +150,11 @@ sub makeSubTaskCommand {
     my $saveGood = $self->getProperty("saveGoodBlastFiles");
     my $blastFilePath = $self->getProperty("blastFileDirPath");
     my $doNotExitOnBlastFailure = $self->getProperty("doNotExitOnBlastFailure");
-    my $printSimSeqsFile = $self->getProperty("$printSimSeqsFile") if $self->getProperty("$printSimSeqsFile");
     my $dbFile = $node->getDir() . "/" . basename($dbFilePath);
 
     my $cmd =  "blastSimilarity  --blastBinDir $blastBin --database $dbFile --seqFile $nodeExecDir/seqsubset.fsa --lengthCutoff $lengthCutoff --pValCutoff $pValCutoff --percentCutoff $percentCutoff --blastProgram $blastProgram --blastVendor $blastVendor --regex $regex --blastParamsFile $nodeExecDir/$blastParamsFile".($saveGood =~ /yes/i ? " --saveGoodBlastFiles --blastFileDir $blastFilePath" : "").($doNotExitOnBlastFailure =~ /yes/i ? " --doNotExitOnBlastFailure" : "");
 
-    $cmd .= " --printSimSeqsFile" if $printSimSeqsFile;
+    $cmd .= " --printSimSeqsFile" if $self->getProperty("printSimSeqsFile");
 
     return $cmd;
 }
