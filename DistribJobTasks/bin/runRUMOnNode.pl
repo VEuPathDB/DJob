43aac1621942246c9fc3256e66e21e50cf08e206
Brian Brunk <brunkb@upenn.edu>
2010-08-24 01:17:41 +0000

    using runCmd to catch errors better
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@8565 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/bin/runRUMOnNode.pl
+++ DistribJobTasks/bin/runRUMOnNode.pl
@@ -3,6 +3,7 @@
 use lib "$ENV{GUS_HOME}/lib/perl";
 use strict;
 use Getopt::Long;
+use CBIL::Util::Utils;
 
 open(LOG,">runRumOnNode.log");
 
@@ -39,72 +40,56 @@ $limitNU,$minBlatIdentity,$countMismatches,$alignToTranscriptome,$inputType,$pai
 
 print LOG "starting: ".`date`; 
 
-system("$bowtieExec -a --best --strata -f $genomeBowtieIndex $readsFile -v 3 --suppress 6,7,8 -p 1 > genomeBowtie.out");
+&runCmd("$bowtieExec -a --best --strata -f $genomeBowtieIndex $readsFile -v 3 --suppress 6,7,8 -p 1 > genomeBowtie.out");
 ## check for error and return proper error code if failed
-&handleError($?,"ERROR running genome bowtie");
 print LOG "finished first bowtie run: ".`date`;
 
-system("perl $perlScriptsDir/make_GU_and_GNU.pl genomeBowtie.out btGenomeU btGenomeNU $pairedEnd");
-&handleError($?,"ERROR parsing genome bowtie");
+&runCmd("perl $perlScriptsDir/make_GU_and_GNU.pl genomeBowtie.out btGenomeU btGenomeNU $pairedEnd");
 print LOG "finished parsing genome bowtie run: ".`date`;
 
 if($transcriptBowtieIndex){
-  system("$bowtieExec -a --best --strata -f $transcriptBowtieIndex $readsFile -v 3 --suppress 6,7,8 -p 1 > transcriptBowtie.out");
-  &handleError($?,"ERROR running transcript bowtie");
+  &runCmd("$bowtieExec -a --best --strata -f $transcriptBowtieIndex $readsFile -v 3 --suppress 6,7,8 -p 1 > transcriptBowtie.out");
   print LOG "finished second bowtie run: ".`date`;
 
-  system("perl $perlScriptsDir/make_TU_and_TNU.pl transcriptBowtie.out $geneAnnotationFile btTranscriptU btTranscriptNU $pairedEnd");
-  &handleError($?,"ERROR parsing transcriptome bowtie run");
+  &runCmd("perl $perlScriptsDir/make_TU_and_TNU.pl transcriptBowtie.out $geneAnnotationFile btTranscriptU btTranscriptNU $pairedEnd");
   print LOG "finished parsing transcriptome bowtie run: ".`date`;
   
-  system("perl $perlScriptsDir/merge_GU_and_TU.pl btGenomeU btTranscriptU btGenomeNU btTranscriptNU bowtieUnique combinedNU $pairedEnd");
-  &handleError($?,"ERROR merging TU and GU");
+  &runCmd("perl $perlScriptsDir/merge_GU_and_TU.pl btGenomeU btTranscriptU btGenomeNU btTranscriptNU bowtieUnique combinedNU $pairedEnd");
   print LOG "finished merging TU and GU: ".`date`;
 
-  system("perl $perlScriptsDir/merge_GNU_and_TNU_and_CNU.pl btGenomeNU btTranscriptNU combinedNU bowtieNU");
-  &handleError($?,"ERROR merging GNU, TNU and CNU");
+  &runCmd("perl $perlScriptsDir/merge_GNU_and_TNU_and_CNU.pl btGenomeNU btTranscriptNU combinedNU bowtieNU");
   print LOG "finished merging GNU, TNU and CNU: ",`date`;
 }else{
-  system("mv btGenomeU bowtieUnique");
-  system("mv btGenomeNU bowtieNU");
+  &runCmd("mv btGenomeU bowtieUnique");
+  &runCmd("mv btGenomeNU bowtieNU");
 }
 
-system("perl $perlScriptsDir/make_unmapped_file.pl $readsFile bowtieUnique bowtieNU blatInput.fa $pairedEnd");
-&handleError($?,"ERROR making R");
+&runCmd("perl $perlScriptsDir/make_unmapped_file.pl $readsFile bowtieUnique bowtieNU blatInput.fa $pairedEnd");
 print LOG "finished making R: ".`date`;
 
-system("$blatExec $genomeFastaFile blatInput.fa blat.out -minIdentity=$minBlatIdentity");
-&handleError($?,"ERROR running first BLAT");
+&runCmd("$blatExec $genomeFastaFile blatInput.fa blat.out -minIdentity=$minBlatIdentity");
 print LOG "finished first BLAT run: ".`date`;
 
-system("$mdustExec blatInput.fa > mdust.out");
-&handleError($?,"ERROR running mdust on R");
+&runCmd("$mdustExec blatInput.fa > mdust.out");
 print LOG "finished running mdust on R: ".`date`;
 
-system("perl $perlScriptsDir/parse_blat_out.pl blatInput.fa blat.out mdust.out blatUnique blatNU".($transcriptBowtieIndex ? "" : " -dna")." -num_insertions_allowed $numInsertions");
-&handleError($?,"ERROR parsing first BLAT run");
+&runCmd("perl $perlScriptsDir/parse_blat_out.pl blatInput.fa blat.out mdust.out blatUnique blatNU".($transcriptBowtieIndex ? "" : " -dna")." -num_insertions_allowed $numInsertions");
 print LOG "finished parsing first BLAT run: ".`date`;
 
-system("perl $perlScriptsDir/merge_Bowtie_and_Blat.pl bowtieUnique blatUnique bowtieNU blatNU merge_Unique_temp merge_NU_temp $pairedEnd");
-&handleError($?,"ERROR merging Bowtie and Blat");
+&runCmd("perl $perlScriptsDir/merge_Bowtie_and_Blat.pl bowtieUnique blatUnique bowtieNU blatNU merge_Unique_temp merge_NU_temp $pairedEnd");
 print LOG "finished merging Bowtie and Blat: ".`date`;
 
-system("perl $perlScriptsDir/RUM_finalcleanup.pl merge_Unique_temp merge_NU_temp merge_Unique_temp2 merge_NU_temp2 $genomeFastaFile -faok".($countMismatches ? " -countmismatches" : ""));
-&handleError($?,"ERROR cleaning up final results");
+&runCmd("perl $perlScriptsDir/RUM_finalcleanup.pl merge_Unique_temp merge_NU_temp merge_Unique_temp2 merge_NU_temp2 $genomeFastaFile -faok".($countMismatches ? " -countmismatches" : ""));
 print LOG "finished cleaning up final results: ".`date`;
 
-system("perl $perlScriptsDir/sort_RUM_small_file.pl merge_Unique_temp2 $mainResultDir/RUM_Unique.$subtaskNumber");
+&runCmd("perl $perlScriptsDir/sort_RUM_small_file.pl merge_Unique_temp2 $mainResultDir/RUM_Unique.$subtaskNumber");
 ##Note am writing these final results directly back to the server ...
-&handleError($?,"ERROR with first sort_RUM_small_file.pl run");
-system("perl $perlScriptsDir/limit_NU.pl merge_NU_temp2 $limitNU > RUM_NU_temp3");
-&handleError($?,"ERROR with limit_NU.pl script");
-system("perl $perlScriptsDir/sort_RUM_small_file.pl RUM_NU_temp3 $mainResultDir/RUM_NU.$subtaskNumber");
-&handleError($?,"ERROR with second sort_RUM.pl run");
+&runCmd("perl $perlScriptsDir/limit_NU.pl merge_NU_temp2 $limitNU > RUM_NU_temp3");
+&runCmd("perl $perlScriptsDir/sort_RUM_small_file.pl RUM_NU_temp3 $mainResultDir/RUM_NU.$subtaskNumber");
 print LOG "finished sorting final results: ".`date`;
 
 if($createSAMFile){
-  system("perl $perlScriptsDir/rum2sam.pl RUM_Unique RUM_NU $readsFile $qualFile $mainResultDir/RUM_sam.$subtaskNumber");
-  &handleError($?,"ERROR creating SAM file");
+  &runCmd("perl $perlScriptsDir/rum2sam.pl RUM_Unique RUM_NU $readsFile $qualFile $mainResultDir/RUM_sam.$subtaskNumber");
   print LOG "finished creating SAM file: ".`date`;
 }
 
@@ -112,10 +97,3 @@ print LOG "runRUMOnNode.pl complete: ".`date`;
 
 close LOG;
 
-sub handleError {
-  my($err,$msg) = @_;
-  if($err){
-    print LOG "$msg\nError Code: $err\n";
-    exit($err);
-  }
-}
