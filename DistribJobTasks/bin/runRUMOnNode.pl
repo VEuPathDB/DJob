15ee71c98f38004a8d89cfe26b659663c93c4ede
Greg Grant <ggrant@pcbi.upenn.edu>
2011-05-21 17:48:42 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@9303 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/bin/runRUMOnNode.pl
+++ DistribJobTasks/bin/runRUMOnNode.pl
@@ -15,10 +15,11 @@ $| = 1;
 
 my ($genomeBowtieIndex,$readsFile,$genomeFastaFile,$transcriptBowtieIndex,
 $geneAnnotationFile,$qualFile,$bowtieExec,$blatExec,$mdustExec,$perlScriptsDir,$fastBlat,
-$limitNU,$minBlatIdentity,$countMismatches,$alignToTranscriptome,$inputType,$pairedEnd,$createSAMFile,$numInsertions,$subtaskNumber,$mainResultDir);
+$limitNU,$minBlatIdentity,$countMismatches,$alignToTranscriptome,$inputType,$pairedEnd,$createSAMFile,$numInsertions,$subtaskNumber,$mainResultDir,$readlength,$minoverlap,$minlength,$match_length_cutoff,$strandSpecific);
 
-&GetOptions("readsFile=s" => \$readsFile, ##??just one fasta file as input??
+&GetOptions("readsFile=s" => \$readsFile,
             "qualFile=s" => \$qualFile, 
+            "readlength=s" => \$readlength, 
             "genomeFastaFile=s" => \$genomeFastaFile, 
             "genomeBowtieIndex=s" => \$genomeBowtieIndex, 
             "transcriptBowtieIndex=s" => \$transcriptBowtieIndex, 
@@ -28,13 +29,16 @@ $limitNU,$minBlatIdentity,$countMismatches,$alignToTranscriptome,$inputType,$pai
             "mdustExec=s" => \$mdustExec, 
             "perlScriptsDir=s" => \$perlScriptsDir, 
             "limitNU=i" => \$limitNU, 
+            "minlength=s" => \$minlength, 
             "pairedEnd=s" => \$pairedEnd,
+            "strandSpecific=s" => \$strandSpecific,
             "minBlatIdentity=i" => \$minBlatIdentity, 
             "numInsertions=i" => \$numInsertions, 
             "createSAMFile=i" => \$createSAMFile, 
             "mainResultDir=s" => \$mainResultDir, 
-            "subtaskNumber=s" => \$subtaskNumber, 
-            "countMismatches=i" => \$countMismatches 
+            "subtaskNumber=s" => \$subtaskNumber,
+            "countMismatches=i" => \$countMismatches,
+            "matchLengthCutoff=i" => \$match_length_cutoff 
             );
 
 
@@ -51,14 +55,18 @@ print LOG "finished parsing genome bowtie run: ".`date` . "$cmd\n\n";
 
 if($transcriptBowtieIndex){
   $cmd = "$bowtieExec -a --best --strata -f $transcriptBowtieIndex $readsFile -v 3 --suppress 6,7,8 -p 1 > transcriptBowtie.out";
-  &runCmd("$bowtieExec -a --best --strata -f $transcriptBowtieIndex $readsFile -v 3 --suppress 6,7,8 -p 1 > transcriptBowtie.out");
+  &runCmd($cmd);
   print LOG "finished second bowtie run: ".`date` . "$cmd\n\n";
 
   $cmd = "perl $perlScriptsDir/make_TU_and_TNU.pl transcriptBowtie.out $geneAnnotationFile btTranscriptU btTranscriptNU $pairedEnd";
   &runCmd($cmd);
   print LOG "finished parsing transcriptome bowtie run: ".`date` . "$cmd\n\n";
   
-  $cmd = "perl $perlScriptsDir/merge_GU_and_TU.pl btGenomeU btTranscriptU btGenomeNU btTranscriptNU bowtieUnique combinedNU $pairedEnd";
+  if($minlength eq 'false') {
+      $cmd = "perl $perlScriptsDir/merge_GU_and_TU.pl btGenomeU btTranscriptU btGenomeNU btTranscriptNU bowtieUnique combinedNU $pairedEnd -readlength $readlength";
+  } else {
+      $cmd = "perl $perlScriptsDir/merge_GU_and_TU.pl btGenomeU btTranscriptU btGenomeNU btTranscriptNU bowtieUnique combinedNU $pairedEnd -readlength $readlength -minoverlap $minlength";
+  }
   &runCmd($cmd);
   print LOG "finished merging TU and GU: ".`date` . "$cmd\n\n";
 
@@ -81,33 +89,77 @@ $cmd = "$mdustExec blatInput.fa > mdust.out";
 &runCmd($cmd);
 print LOG "finished running mdust on R: ".`date` . "$cmd\n\n";
 
-$cmd = "perl $perlScriptsDir/parse_blat_out.pl blatInput.fa blat.out mdust.out blatUnique blatNU".($transcriptBowtieIndex ? "" : " -dna")." -num_insertions_allowed $numInsertions";
+if($minlength eq 'false') {
+    $cmd = "perl $perlScriptsDir/parse_blat_out.pl blatInput.fa blat.out mdust.out blatUnique blatNU".($transcriptBowtieIndex ? "" : " -dna")." -num_insertions_allowed $numInsertions";
+} else {
+    $cmd = "perl $perlScriptsDir/parse_blat_out.pl blatInput.fa blat.out mdust.out blatUnique blatNU -match_length_cutoff $minlength ".($transcriptBowtieIndex ? "" : " -dna")." -num_insertions_allowed $numInsertions";
+}
 &runCmd($cmd);
 print LOG "finished parsing first BLAT run: ".`date` . "$cmd\n\n";
 
-$cmd = "perl $perlScriptsDir/merge_Bowtie_and_Blat.pl bowtieUnique blatUnique bowtieNU blatNU merge_Unique_temp merge_NU_temp $pairedEnd";
+if($minlength eq 'false') {
+    $cmd = "perl $perlScriptsDir/merge_Bowtie_and_Blat.pl bowtieUnique blatUnique bowtieNU blatNU merge_Unique_temp merge_NU_temp $pairedEnd -readlength $readlength";
+} else {
+    $cmd = "perl $perlScriptsDir/merge_Bowtie_and_Blat.pl bowtieUnique blatUnique bowtieNU blatNU merge_Unique_temp merge_NU_temp $pairedEnd -readlength $readlength -minoverlap $minlength";
+}
 &runCmd($cmd);
 print LOG "finished merging Bowtie and Blat: ".`date` . "$cmd\n\n";
 
-$cmd = "perl $perlScriptsDir/RUM_finalcleanup.pl merge_Unique_temp merge_NU_temp merge_Unique_temp2 merge_NU_temp2 $genomeFastaFile "."samheader.txt ".($countMismatches ? " -countmismatches" : "");
+if($minlength eq 'false') {
+    $cmd = "perl $perlScriptsDir/RUM_finalcleanup.pl merge_Unique_temp merge_NU_temp merge_Unique_temp2 merge_NU_temp2 $genomeFastaFile "." $mainResultDir/sam_header.$subtaskNumber ".($countMismatches ? " -countmismatches" : "");
+} else {
+    $cmd = "perl $perlScriptsDir/RUM_finalcleanup.pl merge_Unique_temp merge_NU_temp merge_Unique_temp2 merge_NU_temp2 $genomeFastaFile "." $mainResultDir/sam_header.$subtaskNumber ".($countMismatches ? " -countmismatches" : "") . " -match_length_cutoff $minlength";
+}
 &runCmd($cmd);
 print LOG "finished cleaning up final results: ".`date` . "$cmd\n\n";
 
-$cmd = "perl $perlScriptsDir/sort_RUM.pl merge_Unique_temp2 $mainResultDir/RUM_Unique.$subtaskNumber";
+$cmd = "perl $perlScriptsDir/sort_RUM_by_id.pl merge_NU_temp2 merge_NU_idsorted";
 &runCmd($cmd);
-##Note am writing these final results directly back to the server ...
-$cmd = "perl $perlScriptsDir/limit_NU.pl merge_NU_temp2 $limitNU > RUM_NU_temp3";
-print LOG "finished limiting NU: ".`date` . "$cmd\n\n";
+
+$cmd = "perl $perlScriptsDir/removedups.pl merge_NU_idsorted merge_NU_temp3 merge_Unique_temp2";
 &runCmd($cmd);
-$cmd = "perl $perlScriptsDir/sort_RUM.pl RUM_NU_temp3 $mainResultDir/RUM_NU.$subtaskNumber";
+
+if($limitNU > 0) {
+    $cmd = "perl $perlScriptsDir/limit_NU.pl merge_NU_temp3 $limitNU > $mainResultDir/RUM_NU.$subtaskNumber";
+} else {
+    $cmd = "mv merge_NU_temp3 $mainResultDir/RUM_NU.$subtaskNumber";
+}
+&runCmd($cmd);
+
+$cmd = "perl $perlScriptsDir/sort_RUM_by_id.pl merge_Unique_temp2 $mainResultDir/RUM_Unique.$subtaskNumber";
 &runCmd($cmd);
 print LOG "finished sorting final results: ".`date` . "$cmd\n\n";
 
+##Note am writing these final results directly back to the server ...
+$cmd = "perl $perlScriptsDir/limit_NU.pl merge_NU_temp2 $limitNU > merge_NU_temp3";
+print LOG "finished limiting NU: ".`date` . "$cmd\n\n";
+&runCmd($cmd);
+
 if($createSAMFile){
-$cmd = "perl $perlScriptsDir/rum2sam.pl RUM_Unique RUM_NU $readsFile $qualFile $mainResultDir/RUM_sam.$subtaskNumber";
+$cmd = "perl $perlScriptsDir/rum2sam.pl $mainResultDir/RUM_Unique.$subtaskNumber $mainResultDir/RUM_NU.$subtaskNumber $readsFile $qualFile $mainResultDir/RUM_sam.$subtaskNumber";
   &runCmd($cmd);
   print LOG "finished creating SAM file: ".`date` . "$cmd\n\n";
 }
+
+$cmd = "perl $perlScriptsDir/sort_RUM_by_location.pl $mainResultDir/RUM_Unique.$subtaskNumber $mainResultDir/RUM_Unique.sorted.$subtaskNumber > $mainResultDir/chr_counts_u.$subtaskNumber";
+&runCmd($cmd);
+$cmd = "perl $perlScriptsDir/sort_RUM_by_location.pl $mainResultDir/RUM_NU.$subtaskNumber $mainResultDir/RUM_NU.sorted.$subtaskNumber > $mainResultDir/chr_counts_nu.$subtaskNumber";
+&runCmd($cmd);
+
+if($strandSpecific eq 'true') {
+    $cmd = "perl $perlScriptsDir/rum2quantifications.pl $geneAnnotationFile $mainResultDir/RUM_Unique.sorted.$subtaskNumber $mainResultDir/RUM_NU.sorted.$subtaskNumber $mainResultDir/quant.ps.$subtaskNumber -countsonly -strand p";
+    &runCmd($cmd);
+    $cmd = "perl $perlScriptsDir/rum2quantifications.pl $geneAnnotationFile $mainResultDir/RUM_Unique.sorted.$subtaskNumber $mainResultDir/RUM_NU.sorted.$subtaskNumber $mainResultDir/quant.ms.$subtaskNumber -countsonly -strand m";
+    &runCmd($cmd);
+    $cmd = "perl $perlScriptsDir/rum2quantifications.pl $geneAnnotationFile $mainResultDir/RUM_Unique.sorted.$subtaskNumber $mainResultDir/RUM_NU.sorted.$subtaskNumber $mainResultDir/quant.pa.$subtaskNumber -countsonly -strand p -anti";
+    &runCmd($cmd);
+    $cmd = "perl $perlScriptsDir/rum2quantifications.pl $geneAnnotationFile $mainResultDir/RUM_Unique.sorted.$subtaskNumber $mainResultDir/RUM_NU.sorted.$subtaskNumber $mainResultDir/quant.ma.$subtaskNumber -countsonly -strand m -anti";
+    &runCmd($cmd);
+} else {
+    $cmd = "perl $perlScriptsDir/rum2quantifications.pl $geneAnnotationFile $mainResultDir/RUM_Unique.sorted.$subtaskNumber $mainResultDir/RUM_NU.sorted.$subtaskNumber $mainResultDir/quant.$subtaskNumber -countsonly";
+    &runCmd($cmd);
+}
+
 
 print LOG "runRUMOnNode.pl complete: ".`date`;
 
