098466b882e031fb642dd19c0ae926a0c6b144c7
Steve Fischer <sfischer@upenn.edu>
2012-02-27 19:31:20 +0000

    use -q option
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@10300 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJob/lib/perl/SgeNode.pm
+++ DistribJob/lib/perl/SgeNode.pm
@@ -26,7 +26,7 @@ sub queueNode {
       close R;
       system("chmod +x $runFile");
     }
-    my $qsubcmd = "qsub -V -cwd -pe DJ $self->{procsPerNode} -l h_vmem=$self->{memPerNode}G $runFile";
+    my $qsubcmd = "qsub -V -cwd -pe DJ $self->{procsPerNode} -q $self->{queue} -l h_vmem=$self->{memPerNode}G $runFile";
 #    print "$qsubcmd\n";
 #    my $qsubcmd = "qsub -V -cwd $runFile";
     my $tjid = `$qsubcmd`;
