d5ccc709f79082a9ab1c7da431c17ed7ddd6398e
Brian Brunk <brunkb@upenn.edu>
2012-02-28 21:28:39 +0000

    fixed nodeDir problem
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@10305 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJob/lib/perl/SgeNode.pm
+++ DistribJob/lib/perl/SgeNode.pm
@@ -34,7 +34,7 @@ sub queueNode {
       my $jid = $1;
       $self->setJobid($jid);
 ##NOTE: am changing so that now will use the $TMPDIR for the nodeDir so that PBS will clean up.
-      $self->{nodeDir} = "$ENV{TMPDIR}/$jid";
+      $self->{nodeDir} = "$self->{nodeDir}/$jid";
       if($self->{fileName}){
         open(C,">>$self->{fileName}");
         print C "$self->{jobid} ";
