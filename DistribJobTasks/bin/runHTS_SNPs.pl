6c26fd129c99c7d50e1eaba6da9d062bc44316da
Brian Brunk <brunkb@upenn.edu>
2012-05-15 01:57:39 +0000

    scripts to generate consensus fasta sequence from varscan consensus output
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@10655 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/bin/runHTS_SNPs.pl
+++ DistribJobTasks/bin/runHTS_SNPs.pl
@@ -140,8 +140,8 @@ if(!$snpsOnly){
   }else{ &runCmd($cmd); print "\n"; }
 
   ##now parse to generate the consensus fasta file and gff file of inserts.
-  $cmd = "parseVarscanToConsensus.pl --file $workingDir/$out.varscan.cons --strain $strain --referenceFasta $fastaFile --fastaOutput $workingDir/$out.consensus.fa --indelOutput $workingDir/$out.insertions.GFF >& parseToConsensus.stderr";
-  print L &getDate().": $cmd\n\n";
+  $cmd = "parseVarscanToConsensus.pl --file $workingDir/$out.varscan.cons --strain $strain --referenceFasta $fastaFile --fastaOutput $workingDir/$out.consensus.fa --indelOutput $workingDir/$out.insertions.GFF --percentCutoff $consPercentCutoff >& parseToConsensus.stderr";
+  print L &getDate().": $cmd\n\n;"
   if(-e "$workingDir/complete"){ print L "  succeeded in previous run\n\n";
   }else{ &runCmd($cmd); print "\n"; }
 }
