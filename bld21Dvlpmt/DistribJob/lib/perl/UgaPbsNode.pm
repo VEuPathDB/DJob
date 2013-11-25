0b9d33836523fdabc3ce8286fcf4338fccf0f96e
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2013-11-25 15:52:38 +0000

    creating branch DJob: development for bld 21
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches@13123 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ bld21Dvlpmt/DistribJob/lib/perl/UgaPbsNode.pm
@@ -0,0 +1,108 @@
+package DJob::DistribJob::UgaPbsNode;
+
+use DJob::DistribJob::Node ":states";
+use Cwd;
+use strict;
+
+our @ISA = qw(DJob::DistribJob::Node);
+
+################################################################################
+##NOTE: sites should set the following variable according to how many slots / node
+##      their implementation for PBS has
+my $pbsSlotsPerNode = 1;  
+################################################################################
+
+sub queueNode {
+  my $self = shift;
+  if (!$self->getJobid()) {     ##need to run qsub 
+    ##first create the script...
+    my $runFile = $self->{fileName};
+    if(!$runFile){
+      $runFile = "nodeScript.$$";
+    }elsif($runFile =~ /cancel/){
+      $runFile =~ s/cancel/run/;
+    }else{
+      $runFile = "$runFile.run";
+    }
+    if(!-e "$runFile"){
+      open(R,">$runFile") || die "unable to create script file '$runFile'\n";
+      print R "#!/bin/sh\n\n$ENV{GUS_HOME}/bin/nodeSocketServer.pl $self->{serverHost} $self->{serverPort}\n";
+      close R;
+      system("chmod +x $runFile");
+    }
+    my $qsubcmd = "qsub -N DistribJob -V -j oe -l nodes=1:ppn=$pbsSlotsPerNode".($self->{runTime} ? ",walltime=00:$self->{runTime}:00" : "")." $runFile";
+    my $jid = `$qsubcmd`;
+    chomp $jid;
+    $self->setJobid($jid);
+    if($self->{fileName}){
+      open(C,">>$self->{fileName}");
+      print C "$self->{jobid} ";
+      close C;
+    }
+  } 
+  $self->setState($QUEUED);
+}
+
+sub getNodeAddress {
+  my $self = shift;
+  if (!defined $self->{nodeNum}) {
+    my $getCmd = "qstat -n $self->{jobid}";
+    my @stat = `$getCmd`;
+    return undef if $?;         ##command failed
+    if ($stat[-1] =~ /^\s*(\w*\d+)/) {
+      $self->{nodeNum} = $1;
+    }
+  }
+  return $self->{nodeNum};
+}
+
+sub _initNodeDir {
+  my ($self) = @_;
+  $self->{nodeDir} = $self->{nodeDir} . '/' . $self->{nodeNum};
+  $self->SUPER::_initNodeDir();
+}
+
+##over ride this because want to delete those pesky *.OU files
+sub cleanUp {
+  my ($self,$force, $state) = @_;
+
+  if(!$self->getSaveForCleanup() || ($self->getSaveForCleanup() && !$force)) { 
+    return if $self->getState() >= $COMPLETE; #already cleaned up
+  }
+    
+  if (!$force) {
+    foreach my $slot (@{$self->getSlots()}) {
+      return unless $slot->isFinished();
+    }
+  }
+  
+  ##want to kill any child processes still running to quit cleanly
+  if($self->getState() == $INITIALIZINGTASK && $self->{taskPid}){
+    kill(1, $self->{taskPid}) unless waitpid($self->{taskPid},1);
+  }
+
+  $self->setState($state ? $state : $COMPLETE); ##complete
+
+  ## if saving this one so don't clean up further and release
+  return if($self->getSaveForCleanup() && !$force);  
+  
+  print "Cleaning up node $self->{nodeNum}...\n";
+  
+  if($self->{portCon}){
+    $self->runCmd("/bin/rm -r $self->{nodeDir}", 1);
+    $self->runCmd("closeAndExit");
+    $self->closePort();
+  }else{
+    system("qdel $self->{jobid} > /dev/null 2>&1");
+  }
+  
+  ##delete those pesky OU files that don't do anything
+  my $jid = $self->{jobid};
+  if($self->{jobid} =~ /^(\d+)/){
+    $jid = $1;
+  }
+  my $delCmd = "/bin/rm $ENV{HOME}/$jid.*OU > /dev/null 2>&1";
+  system($delCmd); 
+}
+
+1;
---
+++ bld21Dvlpmt/DistribJobTasks/lib/perl/RepeatMaskerTask.pm
@@ -0,0 +1,88 @@
+package DJob::DistribJobTasks::RepeatMaskerTask;
+
+use DJob::DistribJob::Task;
+use CBIL::Bio::FastaFileSequential;
+use File::Basename;
+use CBIL::Util::Utils;
+
+@ISA = (DJob::DistribJob::Task);
+
+use strict;
+
+# [name, default (or null if reqd), comment]
+my @properties = 
+(
+ ["rmPath",        "default",  "eg: /export/Bioinformatics/usr/local/src/bio/RepeatMasker/latest"],
+ ["inputFilePath", "",  "full path to input file"],
+ ["trimDangling",  "",  "y or n"],
+ ["rmParamsFile",     "",  "full path to file containing RepeatMasker command line options. Can be all on one line or one line / option."],
+ ["dangleMax", "", "trim this or fewer bases"]
+
+ );
+
+sub new {
+    my $self = &DJob::DistribJob::Task::new(@_, \@properties);
+    return $self;
+}
+
+sub initServer {
+    my ($self, $inputDir) = @_;
+
+}
+
+sub initNode {
+    my ($self, $node, $inputDir) = @_;
+
+}
+
+sub getInputSetSize {
+    my ($self, $inputDir) = @_;
+
+    my $fastaFileName = $self->getProperty("inputFilePath");
+
+    if (-e "$fastaFileName.gz") {
+	&runCmd("gunzip $fastaFileName.gz");
+    }
+
+    print "Counting sequences in $fastaFileName\n";
+    $self->{fastaFile} = CBIL::Bio::FastaFileSequential->new($fastaFileName);
+    return $self->{fastaFile}->getCount();
+}
+
+sub initSubTask {
+    my ($self, $start, $end, $node, $inputDir, $subTaskDir, $nodeSlotDir,$subTask) = @_;
+
+    if(!$subTask->getRedoSubtask()){
+      $self->{fastaFile}->writeSeqsToFile($start, $end, 
+					"$subTaskDir/seqsubset.fsa");
+
+      my $rmParamsFile = $self->getProperty("rmParamsFile");
+      &runCmd("cp $inputDir/$rmParamsFile $subTaskDir");
+    }
+    $node->runCmd("touch $subTaskDir/seqsubset.fsa.touch",1);
+    $node->runCmd("/bin/rm $subTaskDir/seqsubset.fsa.touch",1);
+    $node->runCmd("cp -r $subTaskDir/* $nodeSlotDir");
+}
+
+sub makeSubTaskCommand { 
+    my ($self, $node, $inputDir, $nodeExecDir) = @_;
+
+    my $rmPath = $self->getProperty("rmPath");
+    $rmPath = $rmPath eq 'default' ? getProgramDir("RepeatMasker") : $rmPath;
+    my $rmParamsFile = $self->getProperty("rmParamsFile");
+    my $trimDangling = $self->getProperty("trimDangling") eq "y"? "--trimDangling" : "";
+    my $dangleMax = $self->getProperty("dangleMax");
+
+    my $cmd = "repeatMasker --rmPath $rmPath --seqFile seqsubset.fsa --outFile blocked.seq --errorFile blocked.err $trimDangling --dangleMax $dangleMax --rmParamsFile $rmParamsFile";
+
+    return $cmd;
+}
+
+sub integrateSubTaskResults {
+    my ($self, $subTaskNum, $node, $nodeExecDir, $mainResultDir) = @_;
+
+    $node->runCmd("cat $nodeExecDir/blocked.seq >> $mainResultDir/blocked.seq");
+    return 1 if $node->getErr();
+    $node->runCmd("cat $nodeExecDir/blocked.err >> $mainResultDir/blocked.err");
+}
+1;
