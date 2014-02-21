9d699daf6e2be7d49fb77b11d596652f1d7c71bd
Wei Li <weili1@pennmedicine.upenn.edu>
2014-02-21 13:56:14 +0000

    creating branch DJob: branch trunk code for use with icmer db instance as of 2/21/2014
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches@13441 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ icemr_022114/DistribJobTasks/lib/perl/BowtieMappingTask.pm
@@ -0,0 +1,93 @@
+package DJob::DistribJobTasks::BowtieMappingTask;
+
+use DJob::DistribJob::Task;
+use CBIL::Bio::FastaFileSequential;
+use File::Basename;
+use Cwd;
+use CBIL::Util::Utils;
+
+@ISA = (DJob::DistribJob::Task);
+
+use strict;
+
+# [name, default (or null if reqd), comment]
+my @properties = 
+(
+	["mateA", "none", "full path to file of reads"],
+	["mateB", "none", "full path to file of paired ends reads"],
+	["bowtieIndex", "none", "full path of the bowtie indices .. likely same as fasta file"],
+	["isColorspace", "false", "input sequence reads are in SOLiD colorspace.  Quality files must be exactly matename.qual"],
+    ["removePCRDuplicates", "true", "remove PCR duplicates for any analysis involving read depth, e.g., ploidy, CNV, mapping replication origins"],
+    ["extraBowtieParams", "none", "Bowtie parameters other than default"],
+	["bowtie2", "default", "full path to the bowtie2 bin dir"],
+	["sampleName", "", "strain to be put into output"],
+	["deleteIntermediateFiles", "true", "[true]|false: if true then deletes intermediate files to save space"]
+);
+
+sub new {
+    my $self = &DJob::DistribJob::Task::new(@_, \@properties);
+    return $self;
+}
+
+# called once 
+sub initServer {
+  my ($self, $inputDir) = @_;
+}
+
+sub initNode {
+    my ($self, $node, $inputDir) = @_;
+}
+
+sub getInputSetSize {
+    my ($self, $inputDir) = @_;
+    return 1;
+}
+
+sub initSubTask {
+    my ($self, $start, $end, $node, $inputDir, $serverSubTaskDir, $nodeExecDir,$subTask) = @_;
+}
+
+sub makeSubTaskCommand { 
+    my ($self, $node, $inputDir, $nodeExecDir) = @_;
+
+    my $mateA = $self->getProperty ("mateA");
+    my $mateB = $self->getProperty ("mateB");
+    my $bowtieIndex = $self->getProperty ("bowtieIndex");
+    my $isColorspace = $self->getProperty ("isColorspace");
+    my $removePCRDuplicates = $self->getProperty ("removePCRDuplicates");
+    my $extraBowtieParams = $self->getProperty ("extraBowtieParams");
+    my $sampleName = $self->getProperty ("sampleName");
+    my $wDir = "$node->{masterDir}/mainresult";
+    my $bowtie2 = $self->getProperty ("bowtie2");
+
+
+       
+    
+    my $cmd = "runBowtieMapping.pl --mateA $mateA".(-e "$mateB" ? " --mateB $mateB" : "");
+    $cmd .= " --bowtieIndex $bowtieIndex";
+    $cmd .= " --bowtie2 $bowtie2";
+    if($self->getProperty('extraBowtieParams') ne 'none'){
+      $cmd .= "--extraBowtieParams $extraBowtieParams";
+    }
+    if($self->getProperty('isColorspace') eq 'true'){
+      $cmd .= " --isColorspace";
+    }
+    if ($self->getProperty('removePCRDuplicates') eq 'true'){
+      $cmd.= " --removePCRDuplicates";
+    }
+    $cmd .= " --sampleName $sampleName";
+    $cmd .= " --workingDir $wDir" . ($self->getProperty('deleteIntermediateFiles') eq 'true' ? " --deleteIntermediateFiles" : "");
+      
+    return $cmd;
+}
+
+##cleanup materDir here and remove extra files that don't want to transfer back to compute node
+sub integrateSubTaskResults {
+    my ($self, $subTaskNum, $node, $nodeExecDir, $mainResultDir) = @_;
+}
+
+sub cleanUpServer {
+  my($self, $inputDir, $mainResultDir, $node) = @_;
+  }
+
+1;
