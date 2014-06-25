72ff80275027443a0a834d0f27771eeac0664f30
John Brestelli <jbrestell@gmail.com>
2014-06-25 17:18:16 +0000

    creating branch DJob: branch for prism website development
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches@14138 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ prism_06252014/DistribJobTasks/lib/perl/GenomeAlignWithGfClientTask.pm
@@ -0,0 +1,119 @@
+package DJob::DistribJobTasks::GenomeAlignWithGfClientTask;
+
+use DJob::DistribJob::Task;
+use CBIL::Bio::FastaFileSequential;
+use File::Basename;
+use Cwd;
+use CBIL::Util::Utils;
+
+@ISA = (DJob::DistribJob::Task);
+
+use strict;
+
+# [name, default (or null if reqd), comment]
+my @properties = 
+(
+ ["gaBinPath",   "default",   "eg, /genomics/share/bin/blat"],
+ ["targetDirPath",      "",     "full path to directory containing *.2bit file"],
+ ["queryPath",   "",     "full path to input file"],
+ ["queryType", "dna", "type of query ([dna]|prot)"],
+ ["blatParams", "none", "additional params to be passed to blat"],
+ ["maxIntron", "", "the maximum length allowed for gaps that correspond to introns"]
+ );
+
+sub new {
+    my $self = &DJob::DistribJob::Task::new(@_, \@properties);
+    return $self;
+}
+
+# called once 
+sub initServer {
+    my ($self, $inputDir) = @_;
+    my $gaBinPath = $self->getProperty("gaBinPath");
+    die "gaBinPath $gaBinPath doesn't exist" unless ($gaBinPath eq 'default' || -e $gaBinPath);
+    my $targetDirPath = $self->getProperty("targetDirPath");
+    die "targetDirPath $targetDirPath doesn't exist" unless -e $targetDirPath;
+    my $twoBitFile = "$targetDirPath/genomicSeqs.2bit";
+    die "There is no 2bit file: $twoBitFile" unless -e  $twoBitFile;
+}
+
+sub initNode {
+    my ($self, $node, $inputDir) = @_;
+
+    my $targetDirPath = $self->getProperty("targetDirPath");
+    my $nodeDir = $node->getDir();
+
+    my $gaBinPath = $self->getProperty("gaBinPath"); 
+   
+    my $queryType = $self->getProperty("queryType");
+
+
+    my $cmd = "startGfServer --binPath $gaBinPath --nodePort $node->{gfport} --targetDir $targetDirPath".($queryType eq 'prot' ? " --trans" : "");
+
+    print STDERR "$cmd\n";
+    print $node->runCmd($cmd)."\n";
+}
+
+sub getInputSetSize {
+    my ($self, $inputDir) = @_;
+
+    my $fastaFileName = $self->getProperty("queryPath");
+
+    if (-e "$fastaFileName.gz") {
+	&runCmd("gunzip $fastaFileName.gz");
+    }
+
+    print "Creating index for $fastaFileName (may take a while)\n";
+    $self->{fastaFile} = CBIL::Bio::FastaFileSequential->new($fastaFileName);
+
+    return $self->{fastaFile}->getCount();
+}
+
+sub initSubTask {
+    my ($self, $start, $end, $node, $inputDir, $subTaskDir, $nodeSlotDir,$subTask) = @_;
+
+    if(!$subTask->getRedoSubtask()){
+      $self->{fastaFile}->writeSeqsToFile($start, $end, "$subTaskDir/seqsubset.fsa");
+    }
+    $node->runCmd("touch $subTaskDir/seqsubset.fsa.touch",1);
+    $node->runCmd("/bin/rm $subTaskDir/seqsubset.fsa.touch",1);
+
+    $node->runCmd("cp -r $subTaskDir/* $nodeSlotDir");
+}
+
+sub makeSubTaskCommand {
+    my ($self, $node, $inputDir, $nodeExecDir) = @_;
+
+    my $gaBinPath = $self->getProperty("gaBinPath"); #the path of the gfClient script
+    my $targetPath = $self->getProperty("targetDirPath"); #path of the dir with the .2bit file
+    my $port = $node->{gfport};
+    my $nodeDir = $node->getDir();
+    my $maxIntron = $self->getProperty("maxIntron");
+    my $queryType = $self->getProperty("queryType");
+    my $dbType = $queryType eq 'dna' ? 'dna' : 'dnax';
+    my $tmpBlatParams = $self->getProperty('blatParams');
+    my $blatParams = $tmpBlatParams eq 'none' ? "" : $tmpBlatParams;
+
+    my $cmd = ($gaBinPath eq 'default' ? "" : "$gaBinPath/")."gfClient -nohead -maxIntron=$maxIntron -t=$dbType -q=$queryType -dots=10 $blatParams localhost $port $targetPath seqsubset.fsa out.psl";
+
+    return $cmd;
+}
+
+sub integrateSubTaskResults {
+    my ($self, $subTaskNum, $node, $nodeExecDir, $mainResultDir) = @_;
+
+    $node->runCmd("cat $nodeExecDir/out.psl >> $mainResultDir/out.psl");
+    return 1 if $node->getErr();
+}
+
+sub cleanUpNode {
+    my ($self,$node) = @_;
+
+    my $port = $node->{gfport};
+    my $gaBinPath = $self->getProperty('gaBinPath');
+
+    $node->runCmd(($gaBinPath eq 'default' ? "" : "$gaBinPath/")."gfServer stop localhost $port");
+}
+
+
+1;
---
+++ prism_06252014/DistribJobTasks/lib/perl/SimpleTask.pm
@@ -0,0 +1,93 @@
+package DJob::DistribJobTasks::SimpleTask;
+
+use DJob::DistribJob::Task;
+use CBIL::Util::Utils;
+
+@ISA = (DJob::DistribJob::Task);
+
+use strict;
+
+# [name, default (or null if reqd), comment]
+my @properties = 
+(
+ ["cmd",    "",     "fully specified command to execute"],
+ ["debug",  "false", ""],
+ );
+
+sub new {
+    my $self = &DJob::DistribJob::Task::new(@_, \@properties);
+    my $debug = $self->getProperty("debug");
+    $self->{'debug'} = 1 if $debug =~ /true/i;
+    return $self;
+}
+
+# called once
+sub initServer {
+    my ($self, $inputDir) = @_;
+    # do nothing
+}
+
+sub initNode {
+    my ($self, $node, $inputDir) = @_;
+    my $nodeDir = $node->getDir();
+    print "DEBUG: SimpleTask::initNode(node=$node, nodeDir=$nodeDir, inputDir=$inputDir)\n"
+	if $self->{'debug'};
+
+    my $cmd = "cp -r $inputDir/* $nodeDir";
+    print "DEBUG: SimpleTask::initNode: running command $cmd...\n" if $self->{'debug'};
+    $node->runCmd("$cmd");
+    print "DEBUG: done\n" if $self->{'debug'};
+}
+
+sub getInputSetSize {
+    my ($self, $inputDir) = @_;
+
+    return 1;
+}
+
+sub initSubTask {
+    my ($self, $start, $end, $node, $inputDir, $subTaskDir, $nodeSlotDir,$subTask) = @_;
+    print "DEBUG: SimpleTask::initSubTask(start=$start, end=$end, node=$node, "
+	. "inputDir=$inputDir, subTaskDir=$subTaskDir, nodeSlotDir=$nodeSlotDir)\n"
+	if $self->{'debug'};
+
+    ##need to copy work to be doneinto $subTaskDir before copying to node
+    my $cmd = "cp -r $subTaskDir/* $nodeSlotDir";
+    print "DEBUG: SimpleTask::initSubTask: running command $cmd...\n" if $self->{'debug'};
+    $node->runCmd("$cmd");
+    print "DEBUG: done\n" if $self->{'debug'};
+}
+
+sub makeSubTaskCommand { 
+    my ($self, $node, $inputDir, $nodeExecDir) = @_;
+
+    my $cmd = $self->getProperty("cmd");
+    my $nodeDir = $node->getDir();
+
+    print "DEBUG: SimpleTask::runSubTask: command from prop is $cmd...\n" if $self->{'debug'};
+    $cmd =~ s/\$nodeDir/$nodeDir/g; #substitute nodeDir
+    $cmd =~ s/\\s/ /g; #substiture space
+    print "DEBUG: SimpleTask::runSubTask: running subTask w\/ command $cmd...\n" if $self->{'debug'};
+    return $cmd;
+}
+
+sub integrateSubTaskResults {
+    my ($self, $subTaskNum, $node, $nodeExecDir, $mainResultDir) = @_;
+
+    my $cmd = "cp -r $nodeExecDir/* $mainResultDir";
+    print "DEBUG: SimpleTask::integrateSubTaskResults: running command $cmd...\n" if $self->{'debug'};
+    $node->runCmd("$cmd");
+    $cmd = "mv $mainResultDir/subtask.output $mainResultDir/task.out";
+    &runCmd($cmd) if -f "$mainResultDir/subtask.output";
+    print "DEBUG: done\n" if $self->{'debug'};
+}
+
+
+# a node is now passed in that can be used to run commands on a node using $node->runCmd("cmd")
+# NOTE that in order to use this must add keepNodeForPostProcessing=yes to controller.prop file
+sub cleanUpServer {
+  my($self, $inputDir, $mainResultDir, $node) = @_;
+  return 1;
+}
+
+1;
