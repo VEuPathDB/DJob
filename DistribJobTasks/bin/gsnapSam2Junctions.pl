3f5c343c86a4a74d4b4c8fd9e27d3a2dd93315e8
John Brestelli <jbrestell@gmail.com>
2015-04-29 21:04:22 +0000

    junctions script to use full bam file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches/gus4@16012 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/bin/gsnapSam2Junctions.pl
+++ DistribJobTasks/bin/gsnapSam2Junctions.pl
@@ -6,72 +6,80 @@ use strict;
 
 use Getopt::Long;
 
-my ($minsize, $uniqueFile, $nuFile, $outputFile, $isBam);
+my ($minsize, $samFile, $outputFile, $isBam);
 
 &GetOptions("min_size=i"=> \$minsize,
-            "unique_file=s" => \$uniqueFile,
-            "nu_file=s" => \$nuFile,
+            "input_file=s" => \$samFile,
             "output_file=s" => \$outputFile,
             "is_bam" => \$isBam,
     );
 
 $minsize = 0 unless($minsize);;
 
-unless(-e $uniqueFile && -e $nuFile) {
-  die "usage:  gsnapSam2Junctions.pl --unique_file=s --nu_file=s [--min_size=i]\n";
+unless(-e $samFile) {
+  die "usage:  gsnapSam2Junctions.pl  --input_file=s [--min_size=i]\n";
 }
 
 my %junctions;
 
 
-foreach("$uniqueFile|unique", "$nuFile|nu") {
-
-  my ($samFile, $mapperType) = split(/\|/, $_);
-
-  if($isBam) {
+if($isBam) {
     $samFile = "samtools view $samFile|";
-  }
+}
 
-  open(INFILE, $samFile) or die "\nError: Cannot open '$samFile' for reading\n\n";
+open(INFILE, $samFile) or die "\nError: Cannot open '$samFile' for reading\n\n";
 
-  while(my $line = <INFILE>) {
+while(my $line = <INFILE>) {
     chomp($line);
-
+    
     my ($qname, $flag, $rname, $pos, $mapq, $cigar, $rnext, $pnext, $tlen, $seq, $qual, @tags) = split(/\t/, $line);
 
     next unless($cigar =~ /N/);
 #    $sam_chr =~ s/:[^:]*$//;
-
+    
     my $xs; 
+
+    my $xo;
     foreach my $tag (@tags) {
-      next unless($tag =~ /XS:A:([+-])/);
-      $xs = $1;
+	if($tag =~ /XS:A:([+-])/) {
+	    $xs = $1;
+	}
+	if($tag =~ /XO:Z:(\w\w)/) {
+	    $xo = $1;
+	}
     }
 
     die "XS:A tag not found for read $qname" unless($xs);
+    die "XO:Z tag not found for read $qname" unless($xo);
+
+    my $mapperType = "nu";
+    if($xo eq "CT" || $xo eq "CU" || $xo eq "HT" || $xo eq "HU" || $xo eq "PC" || $xo eq "PI" || $xo eq "PL" || $xo eq "PS" || $xo eq "UT" || $xo eq "UU") {
+	$mapperType = "unique";
+    }
+
 
     my $running_offset = 0;
     while($cigar =~ s/^(\d+)([^\d])//) {
-      my $num = $1;
-      my $type = $2;
-      
+	my $num = $1;
+	my $type = $2;
+	
 	if($type eq 'N') {
-          my$start = $pos + $running_offset;
-          my $end = $start + $num - 1;
-          my $junction = "$rname:$start-$end";
-
-          if($num >= $minsize) {
-            $junctions{$junction}->{$xs}->{$mapperType}++;
-          }
+	    my$start = $pos + $running_offset;
+	    my $end = $start + $num - 1;
+	    my $junction = "$rname:$start-$end";
+	    
+	    if($num >= $minsize) {
+		$junctions{$junction}->{$xs}->{$mapperType}++;
+	    }
 	}
-
-      if($type eq 'N' || $type eq 'M' || $type eq 'D') {
-        $running_offset = $running_offset + $num;
-      }
+	
+	if($type eq 'N' || $type eq 'M' || $type eq 'D') {
+	    $running_offset = $running_offset + $num;
+	}
     }
-  }
-  close(INFILE);
 }
+close(INFILE);
+
 
 
 open(OUT, "> $outputFile") or die "Cannot open file $outputFile for writing: $!";
