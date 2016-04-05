5110b3cd3ea0e7d3c56aade6a90b5b54b1438053
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2016-04-05 12:21:35 +0000

    Add script for CNV distrib job
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches/gus4@18123 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ DistribJobTasks/bin/runCNVTasks.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl
+use lib "$ENV{GUS_HOME}/lib/perl";
+use strict;
+use warnings;
+use Getopt::Long;
+use CBIL::Util::Utils;
+
+my ($genomicSeqsFile, $bamFile, $gtfFile, $samtoolsIndex, $sampleName, $window);
+my $workingDir = ".";
+
+#get args
+&GetOptions(
+            "genomicSeqsFile|g=s"=> \$genomicSeqsFile,
+            "bamFile|b=s"=> \$bamFile,
+            "gtfFile=s"=>\$gtfFile,
+            "samtoolsIndex|s=s"=>\$samtoolsIndex,
+            "workingDir|w=s"=>\$workingDir,
+            "sampleName=s"=>\$sampleName,
+            "window=s"=>\$window,
+            );
+die "Genomic Sequence file not found\n".&getParams() unless -e $genomicSeqsFile;
+die "Bam file not found\n".&getParams() unless -e $bamFile;
+die "Gtf file not found\n" .&getParams() unless -e $gtfFile;
+die "Samtools index not found\n" .&getParams() unless -e $samtoolsIndex;
+die "you must provide a sample name\n".&getParams() unless $sampleName;
+
+#open and select logging fh
+open(L,">>$workingDir/runCNVTasks.log");
+
+select L;
+$| = 1;
+
+print L "runCNVTasks.pl run starting ".&getDate()."\n\n";
+
+my $out = "$workingDir/$sampleName.bed";
+
+
+###run cufflinks###
+print L &getDate(). ": running Cufflinks...\n";
+my $cmd = "cufflinks -u -N -p 4 -b $genomicSeqsFile -G $gtfFile -o $workingDir/Cufflinks $bamFile";
+print L &getDate(). ": $cmd\n";
+if (-e "$workingDir/Cufflinks/genes.fpkm_tracking") {
+    print L "Command succeeded in previous run\n\n";
+} else {
+    &runCmd ($cmd); print L "\n";
+}
+
+###make binned coverage file###
+print L &getDate(). ": creating binned coverage file\n";
+my $bedFile = &createBed($samtoolsIndex, $window, $workingDir);
+unless (-e $bedFile) {
+    die "Bed file $bedFile was not successfully created\n";
+}
+my $coverage = &getCoverage($bedFile, $bamFile, $out);
+
+###tidy up###
+close L;
+&runCmd("/bin/rm $bedFile");
+
+sub createBed {
+    my ($index, $winLen, $workingDir) = @_;
+    my $bedfile = (split /\./, $index)[0]."_$winLen.bed";
+    $bedfile = "$workingDir/$bedfile";
+    open (OUT, ">$bedfile") or die "Cannot write to temporary file\n$!\n";
+    open (IN, "$index") or die "Cannot open samtools index for reading\n$!\n";
+    while (<IN>) {
+        my ($chr, $length, $cumulative, $lineLength, $lineBLength) = split(/\t/, $_);
+        die "Chromosome and length are not defined for line $. in $index. [$chr, $length]\n" unless(defined($chr) && defined($length));
+        for (my $i=1; $i+$winLen<$length; $i+=$winLen){
+            printf OUT "%s\t%d\t%d\n", $chr, $i, $i+$winLen-1;
+        }
+        printf OUT "%s\t%d\t%d\n", $chr, $length-($length % $winLen)+1, $length unless ($length % $winLen==0);
+    }
+    close(IN);
+    close(OUT);
+    return $bedfile
+}
+
+sub getCoverage {
+    my ($bed, $bam, $out) = @_;
+    my @coverageBed = split(/\n/, &runCmd("bedtoolsCoverage -counts -sorted -a $bed -b $bam"));
+    my $totalMapped = runCmd("samtools view -c -F 4 $bam");
+    open (OUT, ">$out") or die "Cannot write output file\n$!\n";
+    foreach (@coverageBed) {
+        my ($chr, $start, $end, $mapped, $numNonZero, $lenB, $propNonZero) = split(/\t/, $_);
+        die "Chromosome, start and end coordinates or number of mapped reads are not defined\n" unless (defined($chr) && defined($start) && defined($end) && defined($mapped));
+        $mapped = $mapped/$totalMapped;
+        printf OUT "%s\t%d\t%d\t%g\n", $chr, $start, $end, $mapped;
+    }
+    close OUT;
+}
+    
+sub getParams {
+  return &getDate().": runCNVTasks.pl ... parameter values:\n\tgenomicSeqsFile=$genomicSeqsFile\n\tbamFile=$bamFile\n\tgtfFile=$gtfFile\n\tsampleName=$sampleName\n\tworkingDir=$workingDir\n\n";
+}
+
+sub getDate {
+  my $date = `date`;
+  chomp $date;
+  return $date;
+}
