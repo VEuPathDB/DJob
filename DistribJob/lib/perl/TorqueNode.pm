19df6e745f86d018e2949fe442ddf807e3e85f76
Brian Brunk <brunkb@upenn.edu>
2016-04-21 19:37:59 +0000

    modified getQueueSubmitCommand
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches/gus4@18287 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJob/lib/perl/TorqueNode.pm
+++ DistribJob/lib/perl/TorqueNode.pm
@@ -95,7 +95,7 @@ sub deleteLogFilesAndTmpDir {
 
 # static method
 sub getQueueSubmitCommand {
-  my ($class, $queue) = @_;
+  my ($self, $queue) = @_;
 
   #return "qsub -V -cwd".$queue ? " -q $queue" : "";
   return "qsub -V -j oe -l nodes=1:ppn=1".($self->{runTime} ? ",walltime=00:$self->{runTime}:00" : "").($self->{queue} ? " -q $self->{queue}" : "");
