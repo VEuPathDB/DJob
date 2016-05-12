cfb71685b068fc1cde814263b41e7656a7ee8e82
Steve Fischer <sfischer@upenn.edu>
2016-05-12 21:10:15 +0000

    fix race condition bug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/branches@18440 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- gus4/DistribJob/lib/perl/Controller.pm
+++ gus4/DistribJob/lib/perl/Controller.pm
@@ -40,6 +40,7 @@ sub new {
   END { 
     $self->cleanupOnExit();
     print  $self->{failures}? "Failed\n" : "Done\n";;
+    sleep(30);   # prevent race condition that we exit before log file update is visible
   }
 
   $self->{runTime} = $runTime;
