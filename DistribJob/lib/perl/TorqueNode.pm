de8b55245033301d792c5002efe7520a669fb61f
Brian Brunk <brunkb@upenn.edu>
2016-11-12 04:07:58 +0000

    fixed static method to getQueueSubmitCommand to properly use the queue ... was hard coded
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@19436 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJob/lib/perl/TorqueNode.pm
+++ DistribJob/lib/perl/TorqueNode.pm
@@ -99,9 +99,9 @@ sub deleteLogFilesAndTmpDir {
 
 # static method
 sub getQueueSubmitCommand {
-  my ($self, $queue, $cmdToSubmit) = @_;
+  my ($class, $queue, $cmdToSubmit) = @_;
 
-  return "echo $cmdToSubmit | qsub -V -j oe -l nodes=1:ppn=1".($self->{queue} ? ":$self->{queue}" : "").($self->{runTime} ? ",walltime=00:$self->{runTime}:00" : "");
+  return "echo $cmdToSubmit | qsub -V -j oe -l nodes=1:ppn=1".($queue ? ":$queue" : ""). ",walltime=480:00:00";
 }
 
 # static method to extract Job Id from the output of the commmand run by getQueueSubmitCommand()
