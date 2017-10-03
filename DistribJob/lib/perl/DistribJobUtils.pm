00951fa3c0fe731137faaa8900fc35e9d64c3a56
Steve Fischer <sfischer@upenn.edu>
2017-10-03 15:46:59 +0000

    debug use of rcfile
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@20928 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJob/lib/perl/DistribJobUtils.pm
+++ DistribJob/lib/perl/DistribJobUtils.pm
@@ -12,10 +12,10 @@ use strict;
 #  - logErr
 
 sub runAndMonitorDistribJob {
-  my (mgr, $user, $submitServer, $transferServer, $jobInfoFile, $logFile, $propFile, $numNodes, $time, $queue, $ppn, $maxMemoryGigs) = @_;
+  my ($mgr, $user, $submitServer, $transferServer, $jobInfoFile, $logFile, $propFile, $numNodes, $time, $queue, $ppn, $maxMemoryGigs) = @_;
 
   # if not already started, start it up 
-  if (!_readInfoFile($mgr, $jobInfoFile, $user, $transferServer) || !$self->_distribJobRunning($jobInfoFile, $user, $submitServer, $transferServer, $mgr->getNodeClass())) {
+  if (!_readInfoFile($mgr, $jobInfoFile, $user, $transferServer) || !distribJobRunning($jobInfoFile, $user, $submitServer, $transferServer, $mgr->getNodeClass())) {
 
     # first see if by any chance we are already done (would happen if somehow the flow lost track of the job)
     return 1 if _checkClusterTaskLogForDone($logFile, $user, $transferServer);
@@ -35,31 +35,44 @@ sub runAndMonitorDistribJob {
 }
 
 sub submitDistribJobToQueue {
-  my (mgr, $user, $submitServer, $transferServer, $jobInfoFile, $logFile, $propFile, $numNodes, $time, $queue, $ppn, $maxMemoryGigs) = @_;
+  my ($mgr, $user, $submitServer, $transferServer, $jobInfoFile, $logFile, $propFile, $numNodes, $time, $queue, $ppn, $maxMemoryGigs, $rcfile) = @_;
 
   my $p = $ppn ? "--ppn $ppn " : "";
 
-  my $distribjobCmd = "\$GUS_HOME/bin/distribjobSubmit $logFile --numNodes $numNodes --runTime $time --propFile $propFile --parallelInit 4 --mpn $maxMemoryGigs --q $queue $p";
+  # if rcfile, these will be overridden
+  my $gushome = '$GUS_HOME';
+  my $bashEnv = "-login";  # read bash env from login user's .bashrc
+
+  if ($rcfile) {
+    $gushome = '\$GUS_HOME'; # it needs to be escaped for rcfile
+    $bashEnv = "-rcfile $rcfile -i";
+  }
+
+  my $distribjobCmd = "$gushome/bin/distribjobSubmit $logFile --numNodes $numNodes --runTime $time --propFile $propFile --parallelInit 4 --mpn $maxMemoryGigs --q $queue $p";
+
   my $submitCmd = $mgr->getNodeClass()->getQueueSubmitCommand($queue, $distribjobCmd);
 
   my $cmd = "mkdir -p distribjobRuns; cd distribjobRuns; $submitCmd ";
 
-  # do the submit on submit server, and capture its output
-  my $jobInfo = $mgr->runSshCmdWithRetries(0, "/bin/bash -login -c \"$cmd\"", "", 1, 0, $user, $submitServer, "");
+  my $fullCmd = "/bin/bash $bashEnv -c \"$cmd\"";
 
-  $mgr->error("Did not get jobInfo back from command:\n $cmd") unless $jobInfo;
+  # do the submit on submit server, and capture its output
+  my $jobInfo = $mgr->runSshCmdWithRetries(0, $fullCmd, "", 1, 0, $user, $submitServer, "");
+
+  $mgr->error("Did not get jobInfo back from command:\n $fullCmd") unless $jobInfo;
 
   # now write the output of submit into jobInfoFile on transfer server
   my $writeCmd = "cat > $jobInfoFile";
 
-  open(F, "| ssh -2 $user\@$transferServer '/bin/bash -login -c \"$writeCmd\"'") || $self->error("Can't open file handle to write job info to transfer server");
+  open(F, "| ssh -2 $user\@$transferServer '/bin/bash -login -c \"$writeCmd\"'") || $mgr->error("Can't open file handle to write job info to transfer server");
   print F $jobInfo;
   close(F);
 
   # read it back to confirm it got there safely
-  my $jobInfoRead = _readInfoFile($jobInfoFile, $user, $transferServer);
+  my $jobInfoRead = _readInfoFile($mgr, $jobInfoFile, $user, $transferServer);
   chomp $jobInfoRead;
-  $mgr->error("Failed writing job info to jobinfo file on cluster.  (Reading it back didn't duplicate what we tried to write)") unless $jobInfo eq $jobInfoRead;
+  chomp $jobInfo;
+  $mgr->error("Failed writing job info to jobinfo file on cluster.  (Reading it back didn't duplicate what we tried to write)\n expected: '$jobInfo' got: '$jobInfoRead'") unless $jobInfo eq $jobInfoRead;
 }
 
 
@@ -67,7 +80,7 @@ sub submitDistribJobToQueue {
 sub distribJobRunning {
   my ($mgr, $jobInfoFile, $user, $submitServer, $transferServer, $nodeClass) = @_;
 
-  my $jobSubmittedInfo = $distribJobReadInfoFile($jobInfoFile, $user, $transferServer);
+  my $jobSubmittedInfo = _readInfoFile($mgr, $jobInfoFile, $user, $transferServer);
 
   $mgr->error("Job info file on cluster does not exist or is empty: $jobInfoFile") unless $jobSubmittedInfo;
 
@@ -76,7 +89,7 @@ sub distribJobRunning {
 
   my $checkStatusCmd = $nodeClass->getCheckStatusCmd($jobId);
 
-  my $jobStatusString = $mgr->_runSshCmdWithRetries(0, $checkStatusCmd, undef, 1, 1, $user, $submitServer, "2>&1");
+  my $jobStatusString = $mgr->runSshCmdWithRetries(0, $checkStatusCmd, undef, 1, 1, $user, $submitServer, "2>&1");
 
   if ($jobStatusString) {
     my ($running, $msg) = $nodeClass->checkJobStatus($jobStatusString, $jobId);
@@ -112,9 +125,4 @@ sub _checkClusterTaskLogForDone {
   return $done && $done =~ /Done/;
 }
 
-
-
-
-
-
-
+1;
