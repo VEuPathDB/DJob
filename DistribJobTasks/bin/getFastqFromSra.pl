d3dac1ae012338362566d3814c3b2fd7f724aa1f
Jane Pulman <jane.pulman@liverpool.ac.uk>
2017-10-09 15:55:05 +0000

    adding hasPairedEnd param
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@20964 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/bin/getFastqFromSra.pl
+++ DistribJobTasks/bin/getFastqFromSra.pl
@@ -6,14 +6,15 @@ use CBIL::Util::Sra;
 use Cwd;
 use Getopt::Long;
 
-my($doNotGetFastq,$workingDir,$readsOne,$readsTwo,$sampleIdList);
+my($doNotGetFastq,$workingDir,$readsOne,$readsTwo,$sampleIdList,$hasPairedEnds);
 
 &GetOptions("doNotGetFastq!" => \$doNotGetFastq,
             "workingDir=s" => \$workingDir,
             "readsOne=s" => \$readsOne,
             "readsTwo=s" => \$readsTwo,
             "sampleIdList=s" => \$sampleIdList,
-            );
+	    "pairs=s" => \$hasPairedEnds,
+           );
 
 my $cwd = getcwd();
 chdir($workingDir) if $workingDir;
@@ -22,6 +23,6 @@ my @tmp;
 foreach my $s (split(/,\s*/,$sampleIdList)){
   push(@tmp,$s);
 }
-&getFastqForSampleIds(\@tmp, "$readsOne", "$readsTwo", $doNotGetFastq);
+&getFastqForSampleIds(\@tmp, "$readsOne", "$readsTwo", $doNotGetFastq, $hasPairedEnds);
 
 chdir($cwd) if $workingDir;
