5ed11a6166dc8dad3155704c68773ae947c15a31
Danielle Callan <dcallan@upenn.edu>
2018-06-13 12:51:03 +0000

    save taxon strings to second file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/DJob/trunk@22654 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DistribJobTasks/bin/merge.R
+++ DistribJobTasks/bin/merge.R
@@ -38,9 +38,12 @@ colnames(taxa)[colnames(taxa) == "rn"] <- "V1"
 
 finalTable <- merge(taxa, asvTable, by = "V1")
 finalTable$V1 <- NULL
-finalTable$taxaString <- paste0(finalTable$Kingdom, ";", finalTable$Phylum, ";", finalTable$Class, ";", finalTable$Order, ";", finalTable$Family, ";", finalTable$Genus, ";", finalTable$Species)
+finalTable$taxaString <- paste0(finalTable$Kingdom, "; ", finalTable$Phylum, "; ", finalTable$Class, "; ", finalTable$Order, "; ", finalTable$Family, "; ", finalTable$Genus, "; ", finalTable$Species)
 finalTable[,c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species") := NULL]
 #moves taxaString col from last to first
 setcolorder(finalTable, c("taxaString", colnames(finalTable)[1:ncol(finalTable)-1]))
+
+#write taxonString to seperate file as well, to be consumed by insertSequenceTaxon plugin
+write.table(finalTable$taxaString, file = file.path(filesDir, "final_taxonString.tab"), quote=FALSE, sep = '\t', col.names = FALSE, row.names=FALSE)
 
 write.table(finalTable, file = file.path(filesDir, "final_featureTable.tab"), quote=FALSE, sep = '\t', col.names = NA)
