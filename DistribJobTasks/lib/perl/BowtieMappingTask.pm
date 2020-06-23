21f7942107175fae41fa4b7b0e16a8bcbeed2cc5
Kathryn Crouch <crouch.k@gmail.com>
2020-06-23 15:01:50 -0400

    Fix typo

--- DistribJobTasks/lib/perl/BowtieMappingTask.pm
+++ DistribJobTasks/lib/perl/BowtieMappingTask.pm
@@ -133,7 +133,7 @@ sub cleanUpServer {
 	  die "Top Level Seq Size FIle $topLevelSeqSizeFile does not exist";
       }
 
-      $self->runCmnOnNode($node, "samtools index $mainResultDir/${sampleName}.bam");
+      $self->runCmdOnNode($node, "samtools index $mainResultDir/${sampleName}.bam");
       $self->runCmdOnNode($node, "bamutils tobedgraph $mainResultDir/${sampleName}.bam >$mainResultDir/${sampleName}.bed");
   }
 
